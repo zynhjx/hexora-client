1d4d630c697e183ed5e389aa0640a38556cd5146
Kyoru <lynxairu.ichiro@gmail.com>
2026-02-22 12:06:55 +0800

    add bg for greeting section, remove borders of each card and change the bg into --bg-light, and add profile drop down

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Header({ isOpen, setIsOpen }) {
 
@@ -14,6 +15,8 @@ function Header({ isOpen, setIsOpen }) {
 	const toggleSidebar = () => {
 		setIsOpen(!isOpen);
 	};
+
+	const [dropdownOpen, setDropdownOpen] = useState(false);
 
 	// const isActive = (path) => location.pathname === path;
 
@@ -46,24 +49,33 @@ function Header({ isOpen, setIsOpen }) {
 						</button>
 					</div>
 
-					<div className="app-header__level" aria-label="User level">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-							<path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
-							<path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
-						</svg>
-						<span className="level-value">Lv {user.level.toLocaleString()}</span>
-					</div>
-
-					<div className="app-header__rank" aria-label="User rank">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-							<path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
-						</svg>
-						<span className="rank-value">{user.rank}</span>
-					</div>
-
-					<button className="app-header__avatar" type="button" aria-label="Open user profile">
+					<button className="app-header__avatar" type="button" aria-label="Open user profile" onClick={() => setDropdownOpen((v) => !v)}>
 						<span>{user.initials}</span>
 					</button>
+
+					{dropdownOpen && (
+						<div className="app-header__dropdown">
+							<div className="dropdown-profile-row">
+								<div className="dropdown-avatar">{user.initials}</div>
+								<div className="dropdown-user-info">
+									<div className="dropdown-user-name">Kyoru</div>
+									<div className="app-header__level" aria-label="User level">
+										{/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+											<path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+											<path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+										</svg> */}
+										<span className="level-value">Lv {user.level.toLocaleString()}</span>
+									</div>
+									<div className="app-header__rank" aria-label="User rank">
+										{/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+											<path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+										</svg> */}
+										<span className="rank-value">{user.rank}</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</header>
